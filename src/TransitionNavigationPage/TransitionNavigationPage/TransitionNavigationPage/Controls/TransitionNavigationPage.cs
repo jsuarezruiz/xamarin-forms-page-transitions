7f8b93e829145011b32387612496476e4c0deeaf
José Fabio Martinez Gonzalez <martinez@adesso-mobile.de>
2018-04-23 18:07:28 +0200

    Added code documentation, fixed a bug in Android TransitionNavigationPageRenderer, added a few methods in TransitionNavigationPageRenderer to let it work for every page and element, added TransitionType.None and TransitionType.Default

--- src/TransitionNavigationPage/TransitionNavigationPage/TransitionNavigationPage/Controls/TransitionNavigationPage.cs
+++ src/TransitionNavigationPage/TransitionNavigationPage/TransitionNavigationPage/Controls/TransitionNavigationPage.cs
@@ -4,13 +4,50 @@ namespace TransitionNavigationPage.Controls
 {
     public enum TransitionType
     {
-        Fade,
-        Flip,
-        Scale,
-        SlideFromLeft,
-        SlideFromRight,
-        SlideFromTop,
-        SlideFromBottom
+        /// <summary>
+        /// Do not animate the transition.
+        /// </summary>
+        None = -1,
+
+        /// <summary>
+        /// Let the OS decide how to animate the transition.
+        /// </summary>
+        Default = 0,
+
+        /// <summary>
+        /// Show a fade transition animation.
+        /// </summary>
+        Fade = 1,
+
+        /// <summary>
+        /// Show a flip transition animation.
+        /// </summary>
+        Flip = 2,
+
+        /// <summary>
+        /// Show a scale transition animation.
+        /// </summary>
+        Scale = 3,
+
+        /// <summary>
+        /// Show a slide form left transition animation.
+        /// </summary>
+        SlideFromLeft = 4,
+
+        /// <summary>
+        /// Show a slide form right transition animation.
+        /// </summary>
+        SlideFromRight = 5,
+
+        /// <summary>
+        /// Show a slide form top transition animation.
+        /// </summary>
+        SlideFromTop = 6,
+
+        /// <summary>
+        /// Show a slide form bottom transition animation.
+        /// </summary>
+        SlideFromBottom = 7
     }
 
     public class TransitionNavigationPage : NavigationPage
@@ -26,12 +63,10 @@ namespace TransitionNavigationPage.Controls
 
         public TransitionNavigationPage() : base()
         {
-     
         }
 
         public TransitionNavigationPage(Page root) : base(root)
         {
- 
         }
     }
 }
